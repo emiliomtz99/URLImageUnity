d7faaa74f9054a57adba03f1127fec2f75f51719
Emilio Martinez <48834796+emiliomtz99@users.noreply.github.com>
2021-03-22 18:02:09 -0600

    Add files via upload

---
+++ webImage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class webImage : MonoBehaviour
+{
+    public RawImage img;
+    public Texture texture;
+    void Start()
+    {
+            StartCoroutine(setImage("https://gogreenfime.000webhostapp.com/1.jpeg"));
+    }
+
+    IEnumerator setImage(string url)
+    {
+        Debug.Log("CALLING");
+        Texture2D texture = null;
+        WWW www = new WWW(url);
+        yield return www;       
+        texture = www.texture;
+        img.texture = texture;
+
+        //gameObject.transform.localScale = new Vector2(3,1);
+
+        
+    }
+}
+
+
